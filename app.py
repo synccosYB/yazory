7a77feee5e4992c89ba58d76b3db0d5c03cf7b4f
Yoel Bochner <yoel@synccos.com>
2026-09-08 11:29:08 -0400

    Fix Stripe embedded Checkout UI mode

--- app.py
+++ app.py
@@ -966,7 +966,7 @@ def create_app(test_config=None):
             line_item['price_data']['recurring'] = {
                 'interval': 'week' if frequency == 'Weekly' else 'month'}
         params = {
-            'ui_mode': 'embedded',
+            'ui_mode': 'embedded_page',
             'mode': 'payment' if frequency == 'One time' else 'subscription',
             'line_items': [line_item], 'metadata': metadata,
             'return_url': absolute_url('stripe_success') + '?session_id={CHECKOUT_SESSION_ID}',
