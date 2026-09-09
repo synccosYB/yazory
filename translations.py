4d26e070d75d5e92800f6fe91adfa30f1cc0ce8d
Yoel Bochner <yoel@synccos.com>
2026-09-08 23:12:19 -0400

    Clarify extended family lineage fields

--- translations.py
+++ translations.py
@@ -6,6 +6,16 @@ _EXTRA = {
         'he': 'חבר מבית הכנסת',
         'yi': 'חבר פון שול',
     },
+    'Mother-in-law’s maiden name': {
+        'en': "Father's father-in-law name",
+        'he': 'שם החותן של האב',
+        'yi': "דעם טאטנ'ס שווערס נאמען",
+    },
+    'Mother-in-law’s family / network': {
+        'en': "Father-in-law's father-in-law name",
+        'he': 'שם החותן של החותן',
+        'yi': "דעם שווער'ס שווערס נאמען",
+    },
 }
 
 def translate(text):
